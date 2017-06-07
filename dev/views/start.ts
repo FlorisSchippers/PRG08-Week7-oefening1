9c4fcaa36b7cba99bf2c404eee73c2ce8e8acdc6
Kokodoko <benni_jolink@hotmail.com>
2017-06-07 18:00:25 +0200

    added example code

--- dev/views/start.ts
+++ dev/views/start.ts
@@ -15,11 +15,23 @@ class Start implements View {
         let btn = document.createElement("startbutton");
         this.div.appendChild(btn);
 
-        // TODO ANIMEER DE START BUTTON EN DE TITLE MET TWEENLITE
-        // TweenLite.set(startDiv, {x:100, y:100});
-        // TweenLite.to(startDiv, 1, { x: 400, y: 400, ease: Bounce.easeOut, onComplete: this.doSomething, onCompleteScope: this });
-
-        // TODO ONCLICK: ROEP SHOWVIEW AAN IN GAME.TS
+        // BTN ONCLICK: ROEP SHOWVIEW AAN IN GAME.TS
         // this.game.showView(new Level(this.game)); 
+        // btn.addEventListener(...);
+
+        // STARTPOSITIE MET TWEENLITE
+        // TweenLite.set(title, {x:134, y:-200});
+        // TweenLite.set(btn, {x:361, y:-200});
+
+        // ANIMATIE ONTWERPEN
+        // ANIMATIETYPE IS 'Bounce', 'Back', 'Cubic', 'Linear'
+        // EASE IS easeIn, easeInOut, easeOut, easeNone
+        // TweenLite.to(title, TIJD, {y: YWAARDE, ease: ANIMATIETYPE.easeOut});
+        // TweenLite.to(btn, TIJD, {delay:PAUZE, y:YWAARDE, ease: ANIMATIETYPE.easeOut});
+
+        // VOORBEELD MET CALLBACK
+        // TweenLite.to(btn, 1, { x: 400, y: 400, ease: Bounce.easeOut, onComplete: this.doSomething, onCompleteScope: this });
+
+        
     }
 }
